89bb40cd6281bcbc2643fe90bc58d26deacf2fec
Anthony <86444119+anthms@users.noreply.github.com>
2023-02-14 16:04:01 -0600

    Create sleep.py

---
+++ sleep.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import time
+import signal
+import sys
+
+def wait():
+try:
+print("Going to sleep now.", file=sys.stderr)
+time.sleep(30)
+print("Woke up.", file=sys.stderr)
+except:
+print("Caught an exception.", file=sys.stderr)
+
+def signal_handler(sig, frame):
+print('Ctrl+C!')
+sys.exit(1)
+
+signal.signal(signal.SIGINT, signal_handler)
+wait()
